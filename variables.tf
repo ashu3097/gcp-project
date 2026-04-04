0d878e202c506f634e315206f4b8305f74c5c0b8
Ashutosh Sharma <ashutoshsharma@Ashutoshs-MacBook-Pro.local>
2026-04-04 18:56:16 +0530

    pushing first draft of vpc module

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "project_id"          { type = string }
+variable "region"              { type = string }
+variable "environment"         { type = string }
+variable "vpc_name" {
+  type    = string
+  default = "main-vpc"
+}
+variable "routing_mode" {
+  type    = string
+  default = "REGIONAL"
+}
+variable "public_subnet_cidr"  { type = string }
+variable "private_subnet_cidr" { type = string }
+variable "enable_cloud_nat"    { type = bool;   default = true }
+variable "enable_flow_logs"    { type = bool;   default = false }
+variable "enable_ssh_firewall" { type = bool;   default = true }
+variable "enable_http_firewall" { type = bool;  default = true }
+variable "ssh_allowed_cidrs"   { type = list(string); default = ["35.235.240.0/20"] }
+variable "nat_min_ports_per_vm" { type = number; default = 64 }
+variable "public_subnet_secondary_ranges" {
+  type    = list(object({ range_name = string; ip_cidr_range = string }))
+  default = []
+}
+variable "private_subnet_secondary_ranges" {
+  type    = list(object({ range_name = string; ip_cidr_range = string }))
+  default = []
+}
